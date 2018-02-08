82b1ecd2647cdfe541ec3502f1616a3d9fe0e0b1
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2018-02-08 03:09:01 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -8,7 +8,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
       t.float :price
       t.boolean :family_friendly
       t.datetime :opening_date
-      t.datetime :closing_time
+      t.datetime :closing_date
       t.text :description
 
       t.timestamps
